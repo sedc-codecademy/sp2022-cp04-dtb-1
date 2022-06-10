39d84b51f7dcdeeac22bf91db2cd15f21ff3a35a
Dragan <dmanaskov@gmail.com>
2022-06-10 19:49:29 +0200

    Add Basic Folder Structure

---
+++ src/js/utils.js
@@ -0,0 +1,8 @@
+//this is a utility js file
+//after creating a new js file place the export here first
+
+//CODE HERE
+export { blogHelper } from "./blog.js"
+export { loginHelper } from "./login.js"
+export { newsletterHelper } from "./newsletter.js"
+
