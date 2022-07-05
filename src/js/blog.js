cdfc4fc452b185ffcdf0ee53cdabadcd65c984cc
Vlatko Nikolovski <nikolovski.vlatko97@gmail.com>
2022-07-05 23:58:45 +0200

    added fetch to carousel

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,3 +1,5 @@
+import {carouselSectionComponent} from "./components/carouselSectionComponents.js"
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -57,7 +59,7 @@ const carouselSection = () => {
 }
 carouselSection();
 
-
+carouselSectionComponent();
 
 export const blogHelper = {
     printName,
